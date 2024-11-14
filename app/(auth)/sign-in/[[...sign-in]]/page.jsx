f729dcd91bb3928e33aa37e4ca80644a8e8d26e5
Priyanka Soni <140409655+priyankasoni930@users.noreply.github.com>
2024-11-14 15:49:25 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,13 +3,9 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <section className="bg-white">
-      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
-          <div className="max-w-xl lg:max-w-3xl">
-            <SignIn />
-          </div>
-        </main>
-      </div>
+      <main className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <SignIn />
+      </main>
     </section>
   );
 }
